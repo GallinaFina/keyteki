bc31089cc51d93ab88e3aa8168ca62ea26e3d9a8
GallinaFina <112827360+GallinaFina@users.noreply.github.com>
2024-12-06 23:24:20 +0100

    Create KeymanderCard.js

---
+++ server/game/KeymanderCard.js
@@ -0,0 +1,38 @@
+const Card = require('./Card.js');
+
+class KeymanderCard extends Card {
+    constructor(owner, cardData) {
+        super(owner, cardData);
+        this.fundingRequired = 2;
+        this.fundingIncrement = 2;
+        this.funds = [];
+        this.refunds = [];
+        this.timesRemoved = 0;
+    }
+
+    getFundingRequired() {
+        return this.fundingRequired + (this.timesRemoved * this.fundingIncrement);
+    }
+
+    addFund(card) {
+        this.funds.push(card);
+    }
+
+    addRefund(card) {
+        this.refunds.push(card);
+    }
+
+    isFullyFunded() {
+        return (this.funds.length + this.refunds.length) >= this.getFundingRequired();
+    }
+
+    onLeavesPlay() {
+        super.onLeavesPlay();
+        this.timesRemoved++;
+        // Convert funds to refunds
+        this.refunds = [...this.refunds, ...this.funds];
+        this.funds = [];
+    }
+}
+
+module.exports = KeymanderCard;
